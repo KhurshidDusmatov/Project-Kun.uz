467525c4406aa1faaf56b1eef5cdc44a75a932e7
Khurshid Dusmatov <dusmatovkhurshid@gmail.com>
2023-04-18 13:11:55 +0500

    All packages created, JWT added, Auth and createProfile method created

---
+++ src/main/java/com/example/repository/ProfileRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import com.example.entity.ProfileEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProfileRepository extends CrudRepository<ProfileEntity, Integer> {
+
+    Optional<ProfileEntity> findByEmailAndPasswordAndVisible(String email, String password, boolean visible);
+}

f32df1fe47483127418d92181774cb0b98823a8f
Khurshid Dusmatov <dusmatovkhurshid@gmail.com>
2023-05-08 12:32:34 +0500

    Security config classes added

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -7,6 +7,7 @@ import com.example.enums.LangEnum;
 import com.example.enums.ProfileRole;
 import com.example.service.ArticleService;
 import com.example.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,11 +24,12 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    @PostMapping({"", "/"})
+    @PostMapping({"", "/private"})
     public ResponseEntity<?> create(@RequestBody @Valid ArticleRequestDTO dto,
-                                    @RequestHeader("Authorization") String authorization) {
-        JwtDTO jwtDTO = JwtUtil.getJwtDTO(authorization, ProfileRole.MODERATOR);
-        return ResponseEntity.ok(articleService.create(dto, jwtDTO.getId()));
+                                    HttpServletRequest request ) {
+        JwtUtil.checkForRequiredRole(request, ProfileRole.MODERATOR);
+        Integer prtId = (Integer) request.getAttribute("id");
+        return ResponseEntity.ok(articleService.create(dto, prtId));
     }
 
     @PutMapping("/update/{id}")
@@ -38,13 +40,13 @@ public class ArticleController {
         return ResponseEntity.ok(articleService.update(dto, id));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/private/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") String id,
                                     @RequestHeader("Authorization") String authorization) {
         JwtUtil.getJwtDTO(authorization, ProfileRole.MODERATOR, ProfileRole.ADMIN);
         return ResponseEntity.ok(articleService.delete(id));
     }
-    @GetMapping("/change-status/{id}")
+    @GetMapping("/private/change-status/{id}")
     public ResponseEntity<?> changeStatus(@PathVariable("id") String id,
                                           @RequestParam String status,
                                           @RequestHeader("Authorization") String authorization) {
@@ -52,42 +54,42 @@ public class ArticleController {
         return ResponseEntity.ok(articleService.changeStatus(ArticleStatus.valueOf(status), id, jwt.getId()));
     }
 
-    @GetMapping("/type/{id}/five")
+    @GetMapping("private/type/{id}/five")
     public ResponseEntity<List<ArticleShortInfoDTO>> get5ByTypeId(@PathVariable("id") Integer id) {
         return ResponseEntity.ok(articleService.getLast5ByTypeId(id));
     }
 
-    @GetMapping("/type/{id}")
+    @GetMapping("private/type/{id}")
     public ResponseEntity<List<ArticleShortInfoDTO>> getNByTypeId(@PathVariable("id") Integer typeId, @RequestParam("limit") Integer limit) {
         return ResponseEntity.ok(articleService.getLastNByTypeId(typeId, limit));
     }
 
-    @PostMapping("/get-last8")
+    @PostMapping("public/get-last8")
     public ResponseEntity<List<ArticleShortInfoDTO>> get8ExceptList(@RequestBody ArticleRequestListDTO dto) {
         return ResponseEntity.ok(articleService.getLast8NotGivenList(dto.getIdList()));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("public/{id}")
     public ResponseEntity<ArticleFullInfoDTO> getById(@PathVariable("id") String id,
                                                       @RequestHeader(value = "Accept-Language", defaultValue = "uz", required = false) LangEnum lang) {
         return ResponseEntity.ok(articleService.getById(id, lang));
     }
-    @GetMapping("/get-last-4")
+    @GetMapping("public/get-last-4")
     public ResponseEntity<List<ArticleShortInfoDTO>> getLast4(@RequestParam("type-id") Integer typeId, @RequestParam("article-id") String articeleId) {
         return ResponseEntity.ok(articleService.getLast4ByType(typeId, articeleId));
     }
 
-    @PostMapping("/get-most-read-articles")
+    @PostMapping("public/get-most-read-articles")
     public ResponseEntity<List<ArticleShortInfoDTO>> getMostReadArticles(@RequestParam("type-id") Integer typeId, @RequestParam("article-id") String articeleId) {
         return ResponseEntity.ok(articleService.getMostReadArticles());
     }
 
-    @GetMapping("/get-article-by-type-and-region")
+    @GetMapping("public/get-article-by-type-and-region")
     public ResponseEntity<List<ArticleShortInfoDTO>> getArticleByTypeAndRegion(@RequestParam("type-id") Integer typeId, @RequestParam("region-id") Integer regionId) {
         return ResponseEntity.ok(articleService.getArticleByTypeAndRegion(typeId, regionId));
     }
 
-    @GetMapping(value = "/pagination-by-region")
+    @GetMapping(value = "public/pagination-by-region")
     public ResponseEntity<?> paginationByRegion(@RequestParam(value = "page", defaultValue = "1") int page,
                                                 @RequestParam(value = "size", defaultValue = "4") int size,
                                                 @RequestParam("regionId") Integer regionId) {
--- src/main/java/com/example/util/JwtUtil.java
+++ src/main/java/com/example/util/JwtUtil.java
@@ -4,6 +4,7 @@ import com.example.dto.jwt.JwtDTO;
 import com.example.enums.ProfileRole;
 import com.example.exps.MethodNotAllowedException;
 import io.jsonwebtoken.*;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Date;
 
@@ -34,24 +35,14 @@ public class JwtUtil {
     }
 
     public static JwtDTO decode(String token) {
-        try {
             JwtParser jwtParser = Jwts.parser();
             jwtParser.setSigningKey(secretKey);
-
             Jws<Claims> jws = jwtParser.parseClaimsJws(token);
-
             Claims claims = jws.getBody();
-
             Integer id = (Integer) claims.get("id");
-
             String role = (String) claims.get("role");
             ProfileRole profileRole = ProfileRole.valueOf(role);
-
             return new JwtDTO(id, profileRole);
-        } catch (JwtException e) {
-            e.printStackTrace();
-        }
-        throw new MethodNotAllowedException("Jwt exception");
     }
     public static String decodeEmailVerification(String token) {
         try {
@@ -87,6 +78,20 @@ public class JwtUtil {
             throw new MethodNotAllowedException("Method not allowed");
         }
         return jwtDTO;
+    }
+
+    public static void checkForRequiredRole(HttpServletRequest request, ProfileRole... roleList) {
+        ProfileRole jwtRole = (ProfileRole) request.getAttribute("role");
+        boolean roleFound = false;
+        for (ProfileRole role : roleList) {
+            if (jwtRole.equals(role)) {
+                roleFound = true;
+                break;
+            }
+        }
+        if (!roleFound) {
+            throw new MethodNotAllowedException("Method not allowed");
+        }
     }
 
 
